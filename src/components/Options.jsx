fd7e7bd0aed9d376a5df08d9c67c41864516cd2a
tazbirristy <tazbirristyweb@gmail.com>
2022-10-12 05:02:51 +0600

    readme file edited

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Options = ({option, index, handleCorrect}) => {
     return (
-        <div className='text-centre bg-purple-200 p-2 m-2'>
+        <div className='text-centre bg-purple-200 hover:bg-purple-400 p-2 m-2'>
             <button onClick={()=>handleCorrect(option)}>{index+1}. {option}</button>
         </div>
     );
